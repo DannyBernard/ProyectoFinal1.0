78032a738d486201d944747ad06ebe4d1c6692c9
DANNY BERNARD <danny.bernard@hotmail.com>
2019-08-31 22:31:33 -0400

    empezamos

---
+++ BLLTests/RepositorioBaseTests.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Entities;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioBaseTests
+    {
+   /*
+        private Usuario GetUsuario()
+        {
+         
+            return usuario;
+                
+        }
+*/
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Usuario usuario = new Usuario();
+           // usuario.UsuarioID = 1;
+            usuario.Nombres = "Juan perez";
+            //usuario.User = "jP001";
+            //usuario.password = "juanperez";
+            //usuario.TipoUsuario = false;
+            RepositorioBase<Usuario> repository = new BLL.RepositorioBase<Usuario>();
+            Assert.IsTrue(repository.Guardar(usuario));
+        }
+    }
+}
---
+++ Entities/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Usuario
+    {
+        [Key]
+        public int UsuarioID { get; set; }
+        public string Nombres { get; set; }
+        public string User { get; set; }
+        public string  password { get; set; }
+        public bool TipoUsuario { get; set; }
+
+        public Usuario()
+        {
+            UsuarioID = 0; 
+            Nombres = string.Empty;
+            User = string.Empty;
+            this.password = string.Empty;
+            TipoUsuario = false;
+        }
+
+        public Usuario(int usuarioID, string nombres, string user, string password, bool tipoUsuario)
+        {
+            UsuarioID = usuarioID;
+            Nombres = nombres;
+            User = user;
+            this.password = password;
+            TipoUsuario = tipoUsuario;
+        }
+    }
+}
